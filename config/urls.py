fb852563da67fdaca50bfc713b81aa7bdff9ee02
2KAY-kat <dambwedesigns@gmail.com>
2026-07-03 11:30:14 +0200

    clean parts and tabs ... tried and previews ..,,, waiting for backend apps

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("patients/", include("patients.urls")),
+    
     path("", RedirectView.as_view(pattern_name="accounts:dashboard", permanent=False)),
 ]
